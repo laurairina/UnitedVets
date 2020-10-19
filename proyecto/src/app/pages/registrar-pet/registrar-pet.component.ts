6fc64ec9d8eff3e5ac42d2c2026905701e3064f1
Paul271993 <paul.muresan1993@gmail.com>
2020-10-19 02:13:57 +0200

    modales modificados

--- proyecto/src/app/pages/registrar-pet/registrar-pet.component.ts
+++ proyecto/src/app/pages/registrar-pet/registrar-pet.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-registrar-pet',
   templateUrl: './registrar-pet.component.html',
   styleUrls: ['./registrar-pet.component.css']
 })
 export class RegistrarPetComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
   }
+  
+
+ 
 
 }
+
+
